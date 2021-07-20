9a8b9a57abcf7328b6248723a2d478a50801a007
Yogita rawat <yogitarawat04@gmail.com>
2021-07-21 01:34:10 +0530

    media query

---
+++ dunder-mifflin/timepass.php
@@ -0,0 +1,27 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    1 <input type="color" name="color" id=""> <br><hr>
+    2 <input type="date" name="date" id=""><br><hr>
+    3 <input type="file" name="date" id=""><br><hr>
+    4 <input type="datetime-local"><br><hr>
+    5 <input type="image"><br><hr>
+    6 <input type="month"><br><hr>
+    7 <input type="range"><br><hr>
+    8 <input type="url"><br><hr>
+    9 <input type="week"><br><hr>
+    10 <input type="password"><br><hr>
+    11 <input type="hidden" name="date" id=""><br><hr>
+</body>
+
+</html>
